335fea2806659d80fab789ba93c9edc98e31e419
FurSwapCharaOFFICIAL <85859686+FurSwapCharaOFFICIAL@users.noreply.github.com>
2025-04-10 21:37:42 -0700

    Update furswap.lua
    added Nightmare Lumi

--- FurSwap/furswap.lua
+++ FurSwap/furswap.lua
@@ -100,6 +100,12 @@ SMODS.Atlas {
 }
 
 SMODS.Atlas {
+	key = 'furswapnmlumi',
+	px = 71,
+	py = 95,
+	path = 'j_furswap_lumi.png'
+
+SMODS.Atlas {
 	key = 'furswapchara',
 	px = 71,
 	py = 95,
@@ -125,6 +131,9 @@ local lumi_blurbs = {
 	'You try getting better? Didn\'t think so, hehe!',
 	'Time to cause chaos (in a cool way)'
 }
+
+---SFX
+SMODS.Sound({key = 'gong', path = 'gong.ogg'})
 
 ---Jokers
 SMODS.Joker {
@@ -188,7 +197,6 @@ SMODS.Joker {
             end
         end
     end
-	
 }
 
 SMODS.Joker {
@@ -245,7 +253,7 @@ SMODS.Joker {
 			name = 'Nightmare FurSwap!Chara',
 			text = {
 				'{C:attention}Lucky{} cards give',
-				'{X:almanac,C:edition,s:2.5}#1#(P+1){} Chips & Mult',
+				'{C:edition,s:2.5}#1#(P+1){} Chips & Mult',
 				'when scored',
 				'{C:inactive}(P = order/position of card in played hand, max. 50)',
 				'{C:cry_ascendant,s:1.5,E:1}#2#',
@@ -330,6 +338,70 @@ SMODS.Joker {
 	}
 
 
+---Jokers
+SMODS.Joker {
+    key = 'furswapnmlumi',
+    loc_txt = {
+        name = 'Luminescent',
+        text = {'{C:money}Retrigger{} all jokers to the left of this one {C:money}#2# time(s){}.',
+			'All jokers to the right of this one give {X:mult,C:white}^#1#{} Mult',
+			faceartby('Quietsuburbs'),
+			originworld('Rain World'),
+			characterby('Luminescent123'),
+			altuni('Indie Cross')
+			},
+    },	
+	atlas = 'furswaplumi',
+    pos = { x = 0, y = 0 },
+	soul_pos = { x = 2, y = 0, extra = { x = 1, y = 0 }},
+	config = {
+		extra = {
+		emult = 2,
+		retriggers = 5
+		}
+	},
+	loc_vars = function(self,info_queue,center)
+		return {vars ={center.ability.extra.Xmult,center.ability.extra.retriggers]}
+		}
+	end,
+	cost = 50,
+	rarity = 'furswap_nightmare',
+	unlocked = true,
+	discovered = true,
+	blueprint_compat = true,
+	eternal_compat = true,
+	perishable_compat = false,
+    calculate = function(self, card, context)
+        --Retriggering
+        if context.retrigger_joker_check and not context.retrigger_joker then
+            --card = This Joker, context.other_card = card it's checking to retrigger
+            if card.T.x + card.T.w / 2 > context.other_card.T.x + context.other_card.T.w / 2 then
+                return {
+                    message = localize("k_again_ex"),
+                    repetitions = card.ability.extra.retriggers,
+                    card = card,
+                }
+            end
+        --E-mult
+        elseif context.other_joker and card ~= context.other_joker then
+            if card.T.x + card.T.w / 2 < context.other_joker.T.x + context.other_joker.T.w / 2 then
+                if not Talisman.config_file.disable_anims then
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            context.other_joker:juice_up(0.5, 0.5)
+                            return true
+                        end,
+                    }))
+                end
+                return {
+                    message = localize({ type = "variable", key = "a_xmult", vars = { 3 } }),
+                    e_mult = card.ability.extra.emult,
+                }
+            end
+        end
+    end
+}
+
 
 
 
@@ -374,38 +446,57 @@ SMODS.Joker {
 	--	end
 --	end
 --}
-SMODS.Joker {
+
+
+local nmstone_badge = function(self, card, badges)
+	badges[#badges + 1] = create_badge("Supreme Spectral", G.C.CRY_EMBER, G.C.CRY_ASCENDANT, 1.2)
+end
+
+
+SMODS.Consumable {
 	key = 'furswapnightmare',
 	loc_txt = {
 		name = 'Nightmare Stone',
 		text = {
-			'{C:inactive,E:1}The hell\'s this?',
-			'',
-			faceartby('FurSwap!Chara')
+			'Create a {C:cry_ascendant,s:1.5,E:1}Nightmareish{C:attention} Joker{}.',
+			'{C:red}destroy{} all other Jokers, {C:red,s:1.25}including {C:purple,s:1.25}Eternals',
+			'{C:green}Don\'t destroy{} Jokers if you have {C:important,s:1.25}FurSwap!Chara.'
 		}
 	},
+	set = 'Spectral',
 	pos = { x = 0, y = 0 },
-	soul_pos = { x = 1, y = 0 },
-	cost = 15,
-	rarity = 3,
+	soul_pos = { x = 1, y = 0},
+	cost = 25,
 	unlocked = true,
 	discovered = true,
-	debuff_immune = true,
-	blueprint_compat = false,
-	eternal_compat = true,
-	perishable_compat = false,
 	atlas = 'furswapnightmare',
-	add_to_deck = function(self, card, from_debuff)
-		if not from_debuff then
-			ease_ante(1)
-		end
+	set_card_type_badge = nmstone_badge,
+	hidden = true,
+	can_use = function(self, card)
+		return jl.canuse()
 	end,
-	remove_from_deck = function(self, card, from_debuff)
-		if not from_debuff then
-			ease_ante(-1)
+	use = function(self, card, area, copier)
+	use = function(self, card, area, copier)
+		if #SMODS.find_card("j_furswap_furswapchara") <= 0 then
+			Q(function()
+				for k, v in ipairs(G.jokers.cards) do
+						v:destroy()
+					end
+				end
+				return true
+			end, 0.4)
 		end
-	end
+		Q(function()
+			play_sound('furswap_gong')
+			local card = create_card('Joker', G.jokers, nil, 'furswap_nightmare', nil, nil, nil, 'furswap_nightmare')
+			card:add_to_deck()
+			G.jokers:emplace(card)
+			card:juice_up(0.3, 0.5)
+			return true
+		end, 0.75)
+	end,
 }
+
 ----------------
 ---CODING END---
 ----------------
@@ -413,6 +504,4 @@ SMODS.Joker {
 
 ---notes
 ---Lilly wants to kill 2 to 3 jokers, then herself, but let you win the blind instantly.
---- I want to make a blind that debuffs all duplicate jokers
---- I need a sprite for a joker only useful for fusions.
---- I need to find a way to integrate a fusion system
+--- I want to make a blind that debuffs all duplicate jokers.
